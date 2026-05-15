5c1d5954da8aeda039ac731d813094d4909e844e
zhanna-help <zhanna.magazumova@code.berlin>
2026-05-15 16:34:44 +0200

    Check token secret

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 CHAT_ID = os.getenv("CHAT_ID")
 
+print("BOT_TOKEN exists:", BOT_TOKEN is not None)
+
+print("BOT_TOKEN starts with:", BOT_TOKEN[:5] if BOT_TOKEN else "None")
+
 send_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 response = requests.post(send_url, json={
