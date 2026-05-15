e47a2a05aaeeda959c53210ba739407537a97e9e
zhanna-help <zhanna.magazumova@code.berlin>
2026-05-15 16:18:48 +0200

    Fix bot script

--- bot.py
+++ bot.py
@@ -1,58 +1,38 @@
-import requests
-
-from datetime import date, timedelta
-
 import os
+import requests
+from datetime import date
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 CHAT_ID = os.getenv("CHAT_ID")
 
 COUNTRY = "DE"
-
 REMIND_DAYS = list(range(0, 365))
 
-today = date.today()
+send_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
+requests.post(send_url, json={
+    "chat_id": CHAT_ID,
+    "text": "Test: bot is working"
+})
+
+today = date.today()
 year = today.year
 
 url = f"https://date.nager.at/api/v3/PublicHolidays/{year}/{COUNTRY}"
-
 holidays = requests.get(url).json()
 
 for holiday in holidays:
-
     holiday_date = date.fromisoformat(holiday["date"])
-
     days_left = (holiday_date - today).days
 
     if days_left in REMIND_DAYS:
-
         text = (
-
-            f"ACHTUNG: in {days_left} days Feiertag — "
-
+            f"ACHTUNG: in {days_left} days Feiertag - "
             f"{holiday['localName']}.\n"
-
             f"ALLES GESCHLOSSEN"
-
         )
 
-        send_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
         requests.post(send_url, json={
-
             "chat_id": CHAT_ID,
-            send_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-requests.post(send_url, json={
-
-    "chat_id": CHAT_ID,
-
-    "text": "Test: bot is working"
-
-})
-
             "text": text
-
         })
